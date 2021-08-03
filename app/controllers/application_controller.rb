6e717c757ece3bb75169b1ce486ff24b870caa28
Joseph <josephsasson1@gmail.com>
2021-08-03 17:54:38 -0400

    add backend for login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,22 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/companies" do
-    companies = Company.all
+  get "/users" do
+    User.all.to_json
+  end
 
-    companies.to_json(include: :users)
+  get '/users/:id' do
+    user = User.find_by(id: params[:id])
+  end
+
+  post '/sign-in' do
+    # binding.pry
+    user = User.find_by(username: params[:email])
+    if (user[:password] == params[:password])
+      user.to_json
+    else 
+      {error: 'Incorrect Email or Password'}.to_json
+    end
   end
 
 end
