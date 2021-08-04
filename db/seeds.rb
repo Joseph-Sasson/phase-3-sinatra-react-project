10d5ed2a077585dd3e927fa57b2c6741bf6df9d5
Joseph <josephsasson1@gmail.com>
2021-08-04 15:17:05 -0400

    add test to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,7 @@ dc = Company.create(company_name: "DC Comics")
 marvel = Company.create(company_name: "Marvel")
 swim = Company.create(company_name: "AdultSwim")
 monsters = Company.create(company_name: "Monsters Inc")
+test = Company.create(company_name: "Test")
 
 puts "Creating users"
 user1 = User.create(company_id: disney.id, name: "Alice", username: "a_wonderland", password: "madhatter1", company_name: "Disney", email: 'alice@wonderland.com')
@@ -22,5 +23,6 @@ user7 = User.create(company_id: swim.id, name: "Rick Sanchez", username: "pickle
 user8 = User.create(company_id: swim.id, name: "Morty Smith", username: "get_schwifty", password: "jessica1", company_name: "AdultSwim", email: 'morty@adultswim.com')
 user9 = User.create(company_id: monsters.id, name: "James P. Sullivan", username: "sully", password: "scare1", company_name: "Monsters Inc", email: 'sully@monstersinc.com')
 user10 = User.create(company_id: monsters.id, name: "Mike Wazowski", username: "one_eye_mike", password: "celia1", company_name: "Monsters Inc", email: 'mike@montersinc.com')
+user11 = User.create(company_id: test.id, name: "Test", username: "test", password: "test1", company_name: "Test", email: 'test@test.com')
 
 puts "✅ Done seeding!"
