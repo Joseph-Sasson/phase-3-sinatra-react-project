a01eadcdf9a09949dfae94c6fcec00e00927f01b
Roli <rroycehg@gmail.com>
2021-08-03 21:05:24 -0400

    Added patch to controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,30 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/companies" do
-    companies = Company.all
+  get "/users" do
+    users = User.all
 
-    companies.to_json(include: :users)
+    users.to_json(only: [:company_name], include: :users)
   end
 
+  post '/sign-in' do
+    
+    user = User.find_by(username: params[:email])
+      if (user[:password] == params[:password])
+        user.to_json
+      else
+        {error: "Incorrect Email or Password"}.to_json
+      end
+    end
+
+    patch '/users/:id' do
+      user = User.find(params[:id])
+      user.update(
+        name: params[:name],
+        username: params[:username],
+        password: params[:password]
+      )
+
+      user.to_json
+    end
 end
