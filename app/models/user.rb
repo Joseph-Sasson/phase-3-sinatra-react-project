9facbb0ad9602e6842d6641d2a8d9ae13a2baaaa
Jsheumaker1 <Jsheumaker1@gmail.com>
2021-08-05 17:04:39 -0500

    titleize

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   belongs_to :company
 
   def company_name=(name)
-    company = Company.find_or_create_by(company_name: name)
+    company = Company.find_or_create_by(company_name: name.titleize)
     self.company = company 
     self.save
   end
